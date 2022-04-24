7c8e837e3bc3d4e18a0f9e7e1985e8750c187b69
Mangesh2042 <mangesh2042@gmail.com>
2022-04-24 19:26:05 +0530

    toast use navigate

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,22 @@ import './App.css';
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
 import Home from "./pages/Home"
 import EditPage from "./pages/EditorPage"
+import { Toaster } from 'react-hot-toast';
+
 function App() {
   return (
      <>
+     <div>
+       <Toaster position="top-right" toastOptions={{
+         sucess:{
+           theme:{
+             primary:"#4aed88",
+           },
+         },
+       }}>
+       </Toaster>
+       
+     </div>
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<Home/>}></Route>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,37 @@
 import React, { useState } from 'react'
- import {v4 as uuidV4 } from "uuid"
-  
+import {v4 as uuidV4 } from "uuid"
+import toast from 'react-hot-toast'  
+import { useNavigate } from 'react-router-dom'
+
+
 const Home = () => {
    const[id,setId]=useState()
-   const[userName,setUserName]=useState()
-  
+   const navigate=useNavigate()
+   const [userName,setUserName]=useState()
+
     const account=()=>{
     const id=uuidV4()
     console.log(id);
     setId(id)
+    toast.success('Created a new Room')
+  }
+
+  const join=()=>{
+    if(!userName || !id)
+    {
+      toast.error("Room ID and User Name is required")
+      return
+    }
+    else{
+        navigate(`/editor/${id}`,{
+            state:{
+              userName 
+            },
+          });
+      
+    }
+
+    
   }
 
 
@@ -20,7 +43,7 @@ const Home = () => {
           <p>Paste Invtation Room Id</p>
           <input type="text" value={id} placeholder='Room ID'  onChange={(e)=>setId(e.target.value)}/>
           <input type="text" value={userName} placeholder='UserName' onChange={(e)=>setUserName(e.target.value)}/>
-          <button>Join</button>
+          <button onClick={join}>Join</button>
           <button onClick={()=>account()}>Create Account</button>
         </div>
       </div>
